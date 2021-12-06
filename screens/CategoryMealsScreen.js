d6587c868c1758e081e95f78b6f65e27b938b191
Ankit <akjain6067.aj@gmail.com>
2021-12-06 18:53:14 +0530

    Basic app setup done

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const CategoryMealsScreen = (props) => {
+  return (
+    <View>
+      <Text>Categories Meals Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CategoryMealsScreen;
---
+++ screens/FavouriteScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const FavouritesScreen = (props) => {
+  return (
+    <View>
+      <Text>Favourites Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default FavouritesScreen;
